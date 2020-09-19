993f69b591ffeb8fe6d9ae64f7e2af879cf90b3d
AtilioA <atiliodadalto@hotmail.com>
2020-09-19 11:40:33 -0300

    [Dijsktra 2-stack]: create header file

---
+++ list1/dijkstra_2stack/stack.h
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct item Item;
+typedef struct stack Stack;
+
+Stack *create_stack();
+Item *create_item(int, void *);
+
+void push(Stack *, Item *);
+Item *pop(Stack *);
+void print_stack(Stack *);
+
+int is_operator(char *);
+double compute_values(Item *operand1, Item *operand2, Item *operator);
+
+void free_stack(Stack *);
