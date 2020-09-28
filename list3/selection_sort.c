e1e671d21b76d358fdecd68ebec385d548e2ed65
AtilioA <atiliodadalto@hotmail.com>
2020-09-28 09:51:42 -0300

    [Sort] Implement selection sort

--- list3/selection_sort.c
+++ list3/selection_sort.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "item.h"
 
 int *seleciona_menor(int cont_ordenados, int *indice_menor, int *array, int tam_array)
 {
     int *selecionado;
 
-    printf(" ");
     for (int i = cont_ordenados; i < tam_array; i++)
     {
-        if (selecionado == NULL || *selecionado < array[i])
+        if (selecionado == NULL || less(key(*selecionado), key(array[i])))
         {
             selecionado = &array[i];
             *indice_menor = i;
@@ -21,36 +21,28 @@ int *seleciona_menor(int cont_ordenados, int *indice_menor, int *array, int tam_
 
 int main(int argc, char *argv[])
 {
-    int cont_menor = 0, *array, tam_array = 10;
-    array = (int *)malloc(sizeof(int) * tam_array);
+    int N = atoi(argv[1]), cont_menor = 0, tam_array = N;
+    Item *array;
+    array = (Item *)malloc(sizeof(Item) * tam_array);
 
-    int N = atoi(argv[1]);
     srand(time(NULL));
 
-    printf("Numbers:\n");
+    printf("Numbers (keys):\n");
     for (int i = 0; i < N; i++)
     {
-        array[i] = rand() % 1000;
-        printf("%i ", array[i]);
+        array[i] = (Item) rand() % 1000;
+        printf("%i ", key(array[i]));
     }
 
     printf("\nOrdered array:\n");
-    int indice_menor;
+    int indice_menor = 0;
     for (int cont_ordenados = 0; cont_ordenados < tam_array; cont_ordenados++)
     {
-        int swap = array[cont_ordenados];
         int *menor = seleciona_menor(cont_ordenados, &indice_menor, array, tam_array);
-        // printf("\ncont_ordenados: %i\n", cont_ordenados);
-        // printf("swapei %i; ", swap);
-        // printf("indice_menorzei %i\n", indice_menor);
+	exch(array[cont_ordenados],  array[indice_menor]);
 
-        // printf("menorzei %i; ", *menor);
-        array[cont_ordenados] = *menor;
-        printf("%i", array[cont_ordenados]);
-        array[indice_menor] = swap;
-
-        // *menor = swap;
+        printf("%i ", key(array[cont_ordenados]));
     }
 
-    return 1;
+    return 0;
 }
