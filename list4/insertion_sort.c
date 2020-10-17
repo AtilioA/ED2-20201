0767820a42e5c5fac20ea31d30cc1d8ee3522c86
AtilioA <atiliodadalto@hotmail.com>
2020-10-17 13:48:08 -0300

    [Sort] Fix insertion sort
    Adapt main, add "all" recipe to Makefile

--- list4/insertion_sort.c
+++ list4/insertion_sort.c
@@ -2,7 +2,7 @@
 
 void insert_sort(Item *array, int lo, int hi)
 {
-    for (int current_sort = lo + 1; current_sort < hi; current_sort++)
+    for (int current_sort = lo; current_sort < hi; current_sort++)
     {
         for (int scan_array = current_sort; scan_array > 0 && less(array[scan_array], array[scan_array - 1]); scan_array--)
         {
